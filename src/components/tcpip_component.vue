8b62cff7a87dc7c41b4379dd75d4d27babad218d
Ipasky <ipasauriz@gmail.com>
2024-10-20 20:32:25 +0200

    internet protocols changes and tcpip

--- src/components/tcpip_component.vue
+++ src/components/tcpip_component.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="tcpip_container" id="tcpip_container_ID">
-        <div>Aqui va l'animació del tcp ip</div>
+        <div class="tcpip_title">TCP/IP Protocol</div>
+        <div class="tcpip_animation_container">
+            
+        </div>
     </div>
 </template>
   
 <script>
-    export default {};
+    export default {}
 </script>
+
+<style scoped>
+  .tcpip_container{
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.37);
+    backdrop-filter: blur(8px);
+    text-align: justify;
+    text-justify: inter-word;
+  }
+</style>
