fd024dd109b2ce0f94b11a6f11c17f3a177a51b0
Ipasky <ipasauriz@gmail.com>
2024-10-29 17:23:39 +0100

    new changes, gotoSection works!!! movement fluency improvements

--- src/components/tcpip_component.vue
+++ src/components/tcpip_component.vue
@@ -3,23 +3,42 @@
         <div class="tcpip_title">TCP/IP Protocol</div>
         <div class="tcpip_animation_container" ref="tcpip_animation_container" id="tcpip_animation_container_ID">
 
-          <button class="tcpip_play" id="play_ID"> <img src="/src/assets/play_button.png" class="play_button_image"> </button>
-          <button class="tcpip_pause" id="pause_ID"> <img src="/src/assets/pause_button.png" class="pause_button_image"> </button>
-          <button class="tcpip_restart" id="restart_ID"> <img src="/src/assets/restart_button.png" class="restart_button_image"> </button>
-          <button class="tcpip_opcions"> Opcio 1 </button> <!-- Desplegar tota informació -->
-          <button class="tcpip_opcions"> Opcio 2 </button> <!-- Deshabilitar moviment de la camera automatic -->
-          <button class="tcpip_opcions"> Opcio 3 </button> <!-- Control de la velocitat -->
-          <button class="tcpip_opcions"> Opcio 4 </button> <!-- Mode temps real -->
-          <button class="tcpip_opcions"> Opcio 5 </button> <!-- Mode preguntes d'examen -->
+          <div class="buttons_container">
+            <button class="tcpip_play" id="play_ID"> <img src="/src/assets/play_button.png" class="play_button_image"> </button>
+            <button class="tcpip_pause" id="pause_ID"> <img src="/src/assets/pause_button.png" class="pause_button_image"> </button>
+            <button class="tcpip_restart" id="restart_ID"> <img src="/src/assets/restart_button.png" class="restart_button_image"> </button>
+            <button class="tcpip_opcions"> BACK </button> <!-- Desplegar tota informació -->
+            <button class="tcpip_opcions"> Opcio 2 </button> <!-- Deshabilitar moviment de la camera automatic -->
+            <button class="tcpip_opcions"> Opcio 3 </button> <!-- Control de la velocitat -->
+            <button class="tcpip_opcions"> Opcio 4 </button> <!-- Mode temps real -->
+            <button class="tcpip_opcions"> Opcio 5 </button> <!-- Mode preguntes d'examen -->
+          </div>
 
           <div class="tcpip_wrapper_out" ref="tcpip_wrapper_out" id="tcpip_wrapper_out_ID">
             <div class="tcpip_wrapper_in" ref="tcpip_wrapper_in" id="tcpip_wrapper_in_ID">
 
               <div class="tcpip_wrapper_in_left" ref="tcpip_wrapper_in_left" id="tcpip_wrapper_in_left_ID">
-                <div class="terminal_image_container"><img src="/src/assets/terminal_ssh.png" class="terminal_image"></div>
-                <div class="terminal_input_container">
-                  <input class="terminal_input" type="text">
-                  <div class="terminal_fake_cursor"></div>
+                <div class="terminal_image_container" id="terminal_image_container_ID" @click="goto_selected(0)">
+                  <img src="/src/assets/terminal_ssh.png" class="terminal_image">
+                  <div class="terminal_input_container">
+                    <input class="terminal_input" type="text">
+                    <div class="terminal_fake_cursor"></div>
+                  </div>
+                </div>
+
+                <div class="datagrama_img"><img class="datagrama_img_img" src="/src/assets/datagrama.png"></div>
+
+                <div class="tcpip_left_layer_04_container" id="tcpip_left_layer_04_container_ID" @click="goto_selected(4)">
+                  <div class="tcpip_left_layer_04">Application Layer</div>
+                </div>
+                <div class="tcpip_left_layer_03_container" id="tcpip_left_layer_03_container_ID" @click="goto_selected(3)">
+                  <div class="tcpip_left_layer_03">Transpor Layer</div>
+                </div>
+                <div class="tcpip_left_layer_02_container" id="tcpip_left_layer_02_container_ID" @click="goto_selected(2)">
+                  <div class="tcpip_left_layer_02">Internet Layer</div>
+                </div>
+                <div class="tcpip_left_layer_01_container" id="tcpip_left_layer_01_container_ID" @click="goto_selected(1)">
+                  <div class="tcpip_left_layer_01">Link Layer</div>
                 </div>
               </div>
 
@@ -32,6 +51,8 @@
           </div>
 
         </div>
+
+        <div class="tcpip_datagram_container">f</div>
 
         <div class="tcpip_text_container">
           <!-- TEXT DE PROBA AIXO S'HA DE REDACTAR CORRECTAMENT -->
@@ -51,8 +72,81 @@ const tcpip_wrapper_out = ref(null);
 const tcpip_animation_container = ref(null);
 const demoWrapper = ref(null);
 
-onMounted(() => {
+var isDragging = false;
+var startX, startY;
+var scale = 1;
+var totalSumDragX = 0;
+var totalSumDragY = 0;
+var lastDragX = 0;
+var lastDragY = 0;
 
+var dragX = 0;
+var dragY = 0;
+var sumDragX = 0;
+var sumDragY = 0;
+
+/*
+const tcpipwrapp = tcpip_wrapper_out.value;
+const tcpcontainer = tcpip_animation_container.value;
+
+let isDragging = ref(false);
+let startX = ref(0);
+let startY = ref(0);
+let scale = ref(1);
+let totalSumDragX = ref(0);
+let totalSumDragY = ref(0);
+let lastDragX = ref(0);
+let lastDragY = ref(0);
+
+let dragX = ref(0);
+let dragY = ref(0);
+let sumDragX = ref(0);
+let sumDragY = ref(0);
+
+const onMouseDown = (event) => {
+    isDragging = true;
+    startX = event.clientX;
+    startY = event.clientY;
+    lastDragX = 0;
+    lastDragY = 0;
+
+    event.preventDefault();
+};
+
+const onMouseUp = (event) => {
+  isDragging = false;
+
+  event.preventDefault();
+};
+
+const onMouseMove = (event) => {
+  if (isDragging) {
+    dragX = startX - event.clientX;
+    dragY = startY - event.clientY;
+    sumDragX = dragX - lastDragX;
+    sumDragY = dragY - lastDragY;
+    totalSumDragX -= sumDragX / 2;
+    totalSumDragY -= sumDragY / 2;
+    tcpipwrapp.style.transform = `translate(${totalSumDragX}px, ${totalSumDragY}px) scale(${scale})`;
+
+    lastDragX = dragX;
+    lastDragY = dragY;
+  }
+
+  event.preventDefault();
+};
+
+const wheel = (event) => {
+  event.preventDefault();
+  scale += event.deltaY * -0.001;
+  scale = Math.min(Math.max(scale, 0.2), 5); //0.2 --- 2
+  tcpipwrapp.style.transform = `translate(${totalSumDragX}px, ${totalSumDragY}px) scale(${scale})`;
+  
+  event.preventDefault();
+};
+*/
+
+onMounted(() => {
   // Script per controlar la dimensió vertical del container de l'animació
   document.getElementById('tcpip_animation_container_ID').setAttribute("style",  "height: " + (window.innerHeight - 230) + "px");
   window.onresize = function (){
@@ -79,13 +173,7 @@ onMounted(() => {
   const tcpipwrapp = tcpip_wrapper_out.value;
   const tcpcontainer = tcpip_animation_container.value;
 
-  let isDragging = false;
-  let startX, startY;
-  let scale = 1;
-  let totalSumDragX = 0;
-  let totalSumDragY = 0;
-  let lastDragX = 0;
-  let lastDragY = 0;
+
 
   tcpcontainer.addEventListener('mousedown', (event) => {
     isDragging = true;
@@ -101,10 +189,10 @@ onMounted(() => {
 
   tcpcontainer.addEventListener('mousemove', (event) => {
     if (isDragging) {
-      let dragX = startX - event.clientX;
-      let dragY = startY - event.clientY;
-      let sumDragX = dragX - lastDragX;
-      let sumDragY = dragY - lastDragY;
+      dragX = startX - event.clientX;
+      dragY = startY - event.clientY;
+      sumDragX = dragX - lastDragX;
+      sumDragY = dragY - lastDragY;
       totalSumDragX -= sumDragX / 2;
       totalSumDragY -= sumDragY / 2;
       tcpipwrapp.style.transform = `translate(${totalSumDragX}px, ${totalSumDragY}px) scale(${scale})`;
@@ -117,11 +205,32 @@ onMounted(() => {
   tcpcontainer.addEventListener('wheel', (event) => {
     event.preventDefault();
     scale += event.deltaY * -0.001;
-    scale = Math.min(Math.max(scale, 0.7), 3); //0.2 --- 2
+    scale = Math.min(Math.max(scale, 0.2), 5); //0.2 --- 2
     tcpipwrapp.style.transform = `translate(${totalSumDragX}px, ${totalSumDragY}px) scale(${scale})`;
   });
   // ------------------------------------------------
 
+  const animation2 = anime.timeline({
+    loop: false,
+    autoplay: false
+  }).add({
+    targets: '.datagrama_img',
+    translateY: 85,
+    duration: 1000,
+    easing: 'easeInOutSine'
+  }).add({
+    targets: '.datagrama_img',
+    begin: function(anim){
+      document.getElementById('play_ID').style.backgroundColor = 'white';
+      document.getElementById('pause_ID').style.backgroundColor = 'lightgray';
+      animation2.pause();
+    }
+  }).add({
+    targets: '.datagrama_img',
+    translateY: 160,
+    duration: 1000,
+    easing: 'easeInOutSine'
+  });
 
   const animation = anime.timeline({
     loop: false,
@@ -150,7 +259,8 @@ onMounted(() => {
   });
 
   document.querySelector('.tcpip_play').onclick = function(){
-    animation.play();
+    //animation.play();
+    animation2.play();
     document.getElementById('play_ID').style.backgroundColor = 'lightgray';
     document.getElementById('pause_ID').style.backgroundColor = 'white';
   };
@@ -176,6 +286,59 @@ onMounted(() => {
     tcpipwrapp.style.transform = ``;
   };
 });
+
+function goto_selected(type){
+  // He de fer una variable global per una vegada es fa zoom es desactiva unaltre vegada la possibilitat de fer zoom.
+  // Sol si es torna al inici es pot tornar a fer zoom.
+  // Encara falla si abans de clicar movem la el fons.
+  var idName = '';
+  if(type == 4){
+    idName = 'tcpip_left_layer_04_container_ID';
+  } else if (type == 3){
+    idName = 'tcpip_left_layer_03_container_ID';
+  } else if (type == 2){
+    idName = 'tcpip_left_layer_02_container_ID';
+  } else if (type == 1){
+    idName = 'tcpip_left_layer_01_container_ID';
+  } else if (type == 0){
+    idName = 'terminal_image_container_ID';
+  }
+  document.getElementById('tcpip_wrapper_out_ID').style.transition = "all 0.7s ease";
+  var scale_multiplier = (1/scale)
+  var v1 = document.getElementById(idName).getBoundingClientRect()
+  console.log("Vector v1: ", v1.top, v1.left, v1.bottom, v1.right);
+  var v1x = [(scale_multiplier*v1.top), (scale_multiplier*v1.left)];
+  var v1y = [(scale_multiplier*v1.bottom), (scale_multiplier*v1.right)];
+  console.log("v1x: ", v1x, "v1y: ", v1y);
+
+  var v2 = document.getElementById('tcpip_wrapper_out_ID').getBoundingClientRect()
+  console.log("Wrapper v2: ", v2.top, v2.left, v2.bottom, v2.right);
+  var v2x = [(scale_multiplier*v2.top), (scale_multiplier*v2.left)];
+  var v2y = [(scale_multiplier*v2.bottom), (scale_multiplier*v2.right)];
+  console.log("Wrapper v2x: ", v2x, "v2y: ", v2y);
+
+  var vaux_x = [v2x[0] - v1x[0], v2x[1] - v1x[1]];
+  var vaux_y = [v2y[0] - v1y[0], v2y[1] - v1y[1]];
+  console.log("vaux_x: ", vaux_x, "vaux_y: ", vaux_y);
+  var x_sum = vaux_x[0] + vaux_y[0];  
+  var y_sum = vaux_x[1] + vaux_y[1];  
+  console.log("x_sum: ", x_sum, "y_sum: ", y_sum);
+
+  const tcpipwrapp = tcpip_wrapper_out.value;
+  tcpipwrapp.style.transform = `translate(${y_sum}px, ${x_sum}px) scale(${2})`;
+
+  totalSumDragX = y_sum;
+  totalSumDragY = x_sum;
+  scale = 2;
+  lastDragX = 0;
+  dragX = 0;
+  lastDragY = 0;
+  dragY = 0;
+  setTimeout(() => {
+    document.getElementById('tcpip_wrapper_out_ID').style.transition = "";
+  }, 700);
+}
+
 </script>
 
 <style scoped>
@@ -189,7 +352,20 @@ onMounted(() => {
   .tcpip_animation_container{
     user-select: none;
   }
-
+.datagrama_img{
+  user-select: none;
+  position: absolute;
+  height: 70px;
+  top: 95px;
+  left: 120px;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+.datagrama_img_img{
+  height: 100%;
+  user-select: none;
+  width: 100%;
+}
 
 .terminal_input{
   background: none;
@@ -209,7 +385,6 @@ onMounted(() => {
 }
 .tcpip_wrapper_out{
   background-color: rgba(255, 255, 0, 0.249);
-  position: absolute;
   cursor: grab;
   z-index: 0;
   pointer-events: auto;
