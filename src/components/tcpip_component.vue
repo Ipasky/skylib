0b47bbaabaca2b2af60b58207fcfae55b5e479d7
Ipasky <ipasauriz@gmail.com>
2024-11-03 12:03:05 +0100

    new changes

--- src/components/tcpip_component.vue
+++ src/components/tcpip_component.vue
@@ -7,7 +7,7 @@
             <button class="tcpip_play" id="play_ID"> <img src="/src/assets/play_button.png" class="play_button_image"> </button>
             <button class="tcpip_pause" id="pause_ID"> <img src="/src/assets/pause_button.png" class="pause_button_image"> </button>
             <button class="tcpip_restart" id="restart_ID"> <img src="/src/assets/restart_button.png" class="restart_button_image"> </button>
-            <button class="tcpip_opcions"> BACK </button> <!-- Desplegar tota informació -->
+            <button class="tcpip_opcions" @click="restart_view()"><img src="/src/assets/arrow_button.png" class="arrow_button_image"></button> <!-- Desplegar tota informació -->
             <button class="tcpip_opcions"> Opcio 2 </button> <!-- Deshabilitar moviment de la camera automatic -->
             <button class="tcpip_opcions"> Opcio 3 </button> <!-- Control de la velocitat -->
             <button class="tcpip_opcions"> Opcio 4 </button> <!-- Mode temps real -->
@@ -73,6 +73,7 @@ const tcpip_animation_container = ref(null);
 const demoWrapper = ref(null);
 
 var isDragging = false;
+var wasDragging = false;
 var startX, startY;
 var scale = 1;
 var totalSumDragX = 0;
@@ -176,6 +177,7 @@ onMounted(() => {
 
 
   tcpcontainer.addEventListener('mousedown', (event) => {
+    console.log("Dragging: true");
     isDragging = true;
     startX = event.clientX;
     startY = event.clientY;
@@ -185,10 +187,13 @@ onMounted(() => {
 
   tcpcontainer.addEventListener('mouseup', () => {
     isDragging = false;
+    console.log("Dragging: false");
   });
 
   tcpcontainer.addEventListener('mousemove', (event) => {
     if (isDragging) {
+      wasDragging = true;
+      console.log("WasDragging mousemove: ", wasDragging);
       dragX = startX - event.clientX;
       dragY = startY - event.clientY;
       sumDragX = dragX - lastDragX;
@@ -199,6 +204,9 @@ onMounted(() => {
 
       lastDragX = dragX;
       lastDragY = dragY;
+    }
+    else{
+      wasDragging = false;
     }
   });
   
@@ -287,56 +295,67 @@ onMounted(() => {
   };
 });
 
-function goto_selected(type){
-  // He de fer una variable global per una vegada es fa zoom es desactiva unaltre vegada la possibilitat de fer zoom.
-  // Sol si es torna al inici es pot tornar a fer zoom.
-  // Encara falla si abans de clicar movem la el fons.
-  var idName = '';
-  if(type == 4){
-    idName = 'tcpip_left_layer_04_container_ID';
-  } else if (type == 3){
-    idName = 'tcpip_left_layer_03_container_ID';
-  } else if (type == 2){
-    idName = 'tcpip_left_layer_02_container_ID';
-  } else if (type == 1){
-    idName = 'tcpip_left_layer_01_container_ID';
-  } else if (type == 0){
-    idName = 'terminal_image_container_ID';
-  }
-  document.getElementById('tcpip_wrapper_out_ID').style.transition = "all 0.7s ease";
-  var scale_multiplier = (1/scale)
-  var v1 = document.getElementById(idName).getBoundingClientRect()
-  console.log("Vector v1: ", v1.top, v1.left, v1.bottom, v1.right);
-  var v1x = [(scale_multiplier*v1.top), (scale_multiplier*v1.left)];
-  var v1y = [(scale_multiplier*v1.bottom), (scale_multiplier*v1.right)];
-  console.log("v1x: ", v1x, "v1y: ", v1y);
-
-  var v2 = document.getElementById('tcpip_wrapper_out_ID').getBoundingClientRect()
-  console.log("Wrapper v2: ", v2.top, v2.left, v2.bottom, v2.right);
-  var v2x = [(scale_multiplier*v2.top), (scale_multiplier*v2.left)];
-  var v2y = [(scale_multiplier*v2.bottom), (scale_multiplier*v2.right)];
-  console.log("Wrapper v2x: ", v2x, "v2y: ", v2y);
-
-  var vaux_x = [v2x[0] - v1x[0], v2x[1] - v1x[1]];
-  var vaux_y = [v2y[0] - v1y[0], v2y[1] - v1y[1]];
-  console.log("vaux_x: ", vaux_x, "vaux_y: ", vaux_y);
-  var x_sum = vaux_x[0] + vaux_y[0];  
-  var y_sum = vaux_x[1] + vaux_y[1];  
-  console.log("x_sum: ", x_sum, "y_sum: ", y_sum);
-
-  const tcpipwrapp = tcpip_wrapper_out.value;
-  tcpipwrapp.style.transform = `translate(${y_sum}px, ${x_sum}px) scale(${2})`;
-
-  totalSumDragX = y_sum;
-  totalSumDragY = x_sum;
-  scale = 2;
+function restart_view(){
+  scale = 1;
+  totalSumDragX = 0;
+  totalSumDragY = 0;
   lastDragX = 0;
-  dragX = 0;
   lastDragY = 0;
-  dragY = 0;
-  setTimeout(() => {
-    document.getElementById('tcpip_wrapper_out_ID').style.transition = "";
-  }, 700);
+  document.getElementById('tcpip_wrapper_out_ID').style.transform = ``;
+}
+
+function goto_selected(type){
+  if(wasDragging == false){
+    // He de fer una variable global per una vegada es fa zoom es desactiva unaltre vegada la possibilitat de fer zoom.
+    // Sol si es torna al inici es pot tornar a fer zoom.
+    // Encara falla si abans de clicar movem la el fons.
+    var idName = '';
+    if(type == 4){
+      idName = 'tcpip_left_layer_04_container_ID';
+    } else if (type == 3){
+      idName = 'tcpip_left_layer_03_container_ID';
+    } else if (type == 2){
+      idName = 'tcpip_left_layer_02_container_ID';
+    } else if (type == 1){
+      idName = 'tcpip_left_layer_01_container_ID';
+    } else if (type == 0){
+      idName = 'terminal_image_container_ID';
+    }
+    document.getElementById('tcpip_wrapper_out_ID').style.transition = "all 0.7s ease";
+    var scale_multiplier = (1/scale)
+    var v1 = document.getElementById(idName).getBoundingClientRect()
+    //console.log("Vector v1: ", v1.top, v1.left, v1.bottom, v1.right);
+    var v1x = [(scale_multiplier*v1.top), (scale_multiplier*v1.left)];
+    var v1y = [(scale_multiplier*v1.bottom), (scale_multiplier*v1.right)];
+    //console.log("v1x: ", v1x, "v1y: ", v1y);
+
+    var v2 = document.getElementById('tcpip_wrapper_out_ID').getBoundingClientRect()
+    //console.log("Wrapper v2: ", v2.top, v2.left, v2.bottom, v2.right);
+    var v2x = [(scale_multiplier*v2.top), (scale_multiplier*v2.left)];
+    var v2y = [(scale_multiplier*v2.bottom), (scale_multiplier*v2.right)];
+    //console.log("Wrapper v2x: ", v2x, "v2y: ", v2y);
+
+    var vaux_x = [v2x[0] - v1x[0], v2x[1] - v1x[1]];
+    var vaux_y = [v2y[0] - v1y[0], v2y[1] - v1y[1]];
+    //console.log("vaux_x: ", vaux_x, "vaux_y: ", vaux_y);
+    var x_sum = vaux_x[0] + vaux_y[0];  
+    var y_sum = vaux_x[1] + vaux_y[1];  
+    //console.log("x_sum: ", x_sum, "y_sum: ", y_sum);
+
+    const tcpipwrapp = tcpip_wrapper_out.value;
+    tcpipwrapp.style.transform = `translate(${y_sum}px, ${x_sum}px) scale(${2})`;
+
+    totalSumDragX = y_sum;
+    totalSumDragY = x_sum;
+    scale = 2;
+    lastDragX = 0;
+    dragX = 0;
+    lastDragY = 0;
+    dragY = 0;
+    setTimeout(() => {
+      document.getElementById('tcpip_wrapper_out_ID').style.transition = "";
+    }, 700);
+  }
 }
 
 </script>
@@ -355,8 +374,8 @@ function goto_selected(type){
 .datagrama_img{
   user-select: none;
   position: absolute;
-  height: 70px;
-  top: 95px;
+  height: 60px;
+  top: 102px;
   left: 120px;
   transform: translate(-50%, -50%);
   z-index: 1;
